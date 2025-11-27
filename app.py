232a3811c60f6ebd8e9e591a874441649917a0a5
Michael <michaelmawuli18@icloud.com>
2025-11-27 18:11:09 +0000

    Initialize Flask application with a simple route

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Flask is fun to learn"
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
